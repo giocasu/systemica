e523b33861cb5730fe6a06d93e9fbbf647b9da2a
Giovanni Casu <giovanni.casu@gmail.com>
2025-12-28 00:03:56 +0100

    Add MIT license + GitHub Pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
+  base: command === 'build' ? '/prod-cons/' : '/',
   optimizeDeps: {
     exclude: ['quickjs-emscripten']
   },
   build: {
     target: 'esnext'
   }
-});
+}));
